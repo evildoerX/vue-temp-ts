684fb0394a44981e1e049030f6b564d89fa8f7c9
CH <CH@CH.com>
2019-11-11 17:38:53 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import { Component, Vue, Watch } from "vue-property-decorator";
 import "./App.less";
-// import FsUtil from "./farmwork/utils/fs.util";
 @Component({})
 export default class App extends Vue {
-  // public fsUtil: FsUtil = new FsUtil();
-  async created() {
-    // this.fsUtil.init();
-  }
+  async created() {}
 
   private render() {
     return (
