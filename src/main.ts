471877f8699b832f5fde86999e724bb52e182d46
CH <CH@CH.com>
2019-11-11 17:37:18 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import App from "@/App";
+import "@/registerServiceWorker";
+import router from "@/router";
+import store from "@/store";
+import { message } from "ant-design-vue";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+Vue.use(Antd);
+Vue.prototype.$message = message;
 
 Vue.config.productionTip = false;
 
