de7f321b23a26bd3a109f2767c11c12ab94b0cf6
CH <CH@CH.com>
2019-11-11 17:35:59 +0800

    init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,23 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Router from "vue-router";
+import { routes } from "./router";
+Vue.use(Router);
 
-Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
-
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
+const router = new Router({
   routes
+});
+
+// 登陆页面路由 name
+const LOGIN_PAGE_NAME = "login";
+
+// 跳转之前
+router.beforeEach((to, from, next) => {
+  next();
+});
+
+// 跳转之后
+router.afterEach(to => {
+  //
 });
 
 export default router;
---
+++ src/router/router.ts
@@ -0,0 +1,9 @@
+/**
+ * meta 可配置参数
+ * @param {boolean} icon 页面icon
+ * @param {boolean} keepAlive 是否缓存页面
+ * @param {string} title 页面标题
+ */
+export const appRouter = [];
+
+export const routes = [...appRouter];
